6bf895e8ccfbaee8c395b511da9aaf1a628c2a70
Temurbek Sabirov <prodota150@gmail.com>
2020-05-22 19:01:37 -0400

    Add getUser and addUser endpoints

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,3 +1,4 @@
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 import { User } from "../models/user.ts";
 
 let users: User[] = [
@@ -28,13 +29,52 @@ const getUsers = ({ response }: { response: any }) => {
 // @desc Get single user
 // @route GET api/v1/users/:id
 
-const getUser = ({ response }: { response: any }) => {
+const getUser = (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+  const product: User | undefined = users.find((user) =>
+    params.id === params.id
+  );
+
+  if (product) {
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      msg: `No user with id: ${params.id} found`,
+    };
+  }
 };
 
 // @desc Add user
 // @route POST api/v1/users
 
-const addUser = ({ response }: { response: any }) => {
+const addUser = async (
+  { request, response }: { request: any; response: any },
+) => {
+  const body = await request.body();
+
+  if (!request.hasBody) {
+    response.status = 404;
+    response.body = {
+      success: false,
+      msg: "Could not add",
+    };
+  } else {
+    const user: User = body.value;
+    user.id = v4.generate();
+    users.push(user);
+    response.status = 201;
+    response.body = {
+      success: true,
+      data: user,
+    };
+  }
 };
 
 // @desc Update user
